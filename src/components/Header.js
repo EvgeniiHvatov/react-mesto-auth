44d6e9cb58a90ffbb4f090a821c7b4505c34df75
Evgenii Hvatov <hvatovspb@yandex.ru>
2023-07-02 17:41:47 +0300

    feat: change header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import logo from '../images/logo.svg';
+import {Link} from 'react-router-dom';
 
-function Header() {
+
+const Header = ({title, route, email, onClick}) => {
   return (
       <header className="header">
         <div style={{ backgroundImage: `url(${logo})` }} className="header__logo"/>
+          <div className='header__auth'>
+          <p className='header__text'>{email}</p>
+          <Link to={route} className='header__link' onClick={onClick}>{title}</Link>
+        </div>
       </header>
   );
 }
